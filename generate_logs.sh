bee48ff5c0a256207a7e89fcdb5bbf3973a8034a
Adrian Wong <awong@thoughtworks.com>
2017-11-24 11:09:07 +0000

    Change shell script to ruby
    This commit also adds a curl call to find out how long a particular
    artifact from a set of commits sits in aptly for.
    
    The curl doesn't really work with metadata and this commit is more for
    backup, since I will use a new way to determine the duration it sits in
    aptly in the next commit.

--- generate_logs.sh
+++ generate_logs.sh
@@ -7,7 +7,7 @@ repos=$(cat ./repo_list.txt)
 log_file_name="time_from_commit_to_release_logs.csv"
 
 [ -f $log_file_name ] && rm $log_file_name
-echo "Sha,Commit date,Tag date,Difference (hours),Tag,Repo" > $log_file_name
+echo "Sha,Commit date,Tag date,Commit to release prep (hours), Aptly to release prep (hours),Tag,Repo" > $log_file_name
 
 for repo in $repos
 do
@@ -17,7 +17,7 @@ do
     git clone "git@github.com:alphagov/$repo"
     cd "$repo"
   fi
-  ../calculate_difference.sh $repo | tee -a ../$log_file_name
+  ../calculate_difference.rb $repo | tee -a ../$log_file_name
   cd "$curdir"
 done
 
