125397e0de3cc9cd98bec10ef7e11582fa765b35
Adrian Wong <awong@thoughtworks.com>
2017-11-27 16:54:53 +0000

    Clean up script
    Instead of using a curl to get the timestamp of the aptly package, the
    script now finds what the timestamp of a particular build is

--- calculate_difference.rb
+++ calculate_difference.rb
@@ -4,48 +4,32 @@ require 'date'
 require 'time_difference'
 
 STARTDATE = '2017-2-1'
-NUMBEROFCOMMITS = 50
+NUMBEROFCOMMITS = 2000
 
 def get_tag(sha, tag)
-  `git tag --contains #{sha}| grep #{tag} | head -1`.chomp
-end
-
-def construct_build_query_string(repo_name, build_tag_number)
-  matrix = {
-    'doc-checking' => ['dcs-audit-consumer', 'amd64'],
-    'verify-frontend-federation-config' => ['verify-frontend-federation-config', 'all'],
-    'verify-frontend-api' => ['frontend', 'amd64'],
-    'ida-hub' => ['config', 'amd64'],
-    'verify-frontend' => ['front-assets', 'amd64'],
-    'verify-puppet' => ['ida-webops', 'all'],
-    'ida-stub-idp' => ['stub-idp', 'amd64']
-  }
-
-  package_name, suffix = matrix[repo_name]
-  if repo_name == 'verify-frontend'
-    "https://packages.ida.digital.cabinet-office.gov.uk/main/#{package_name[0]}/#{package_name}-#{build_tag_number}/#{package_name}-#{build_tag_number}_1_#{suffix}.deb"
-  else
-    "https://packages.ida.digital.cabinet-office.gov.uk/main/#{package_name[0]}/#{package_name}/#{package_name}_#{build_tag_number}_#{suffix}.deb"
-  end
+  `git tag --contains #{sha}| egrep '#{tag}_\\d{2,}' | sort -t _ -k 2 -g | head -1`.chomp
 end
 
 repo_name = ARGV[0]
 shas = `git log --after=#{STARTDATE} --format=format:%H -#{NUMBEROFCOMMITS}`.split("\n")
 
 shas.each do |sha|
-  release_tag = repo_name == 'verify_puppet' ? get_tag(sha, 'verify_puppet_release') : get_tag(sha, 'release')
+  release_tag = repo_name == 'verify-puppet' ? get_tag(sha, 'verify_puppet_release') : get_tag(sha, 'release')
 
   unless release_tag.empty?
-    build_tag_number = get_tag(sha, 'build').delete("^0-9")
-    build_query_string = construct_build_query_string(repo_name, build_tag_number)
-    build_artifact_date = `curl -sI #{build_query_string} | awk 'NR==6{print $0}' | cut -d ' ' -f2-`.chomp
-
     commit_date = `TZ=UTC git log -1 --format=%cd --date=local #{sha}`.chomp
-    tag_date = `git tag -l --format="%(taggerdate:format:%c)" #{release_tag}`.chomp
 
-    time_from_commit_to_release_prep = TimeDifference.between(DateTime.parse(tag_date), DateTime.parse(commit_date)).in_hours
-    time_from_aptly_to_release_prep = TimeDifference.between(DateTime.parse(tag_date), DateTime.parse(build_artifact_date)).in_hours
+    build_tag = get_tag(sha, 'build')
+    build_tag_number = build_tag.delete("^0-9")
+    build_tag_date = `git tag -l --format="%(taggerdate:format:%c)" #{build_tag}`.chomp
 
-    puts "#{sha},#{commit_date},#{tag_date},#{time_from_commit_to_release_prep},#{time_from_aptly_to_release_prep },#{release_tag},#{repo_name}"
+    release_tag_date = `git tag -l --format="%(taggerdate:format:%c)" #{release_tag}`.chomp
+    parsed_release_tag_date = DateTime.parse(release_tag_date)
+
+    time_from_commit_to_release_prep = TimeDifference.between(parsed_release_tag_date, DateTime.parse(commit_date)).in_hours
+    time_from_aptly_to_release_prep = TimeDifference.between(parsed_release_tag_date, DateTime.parse(build_tag_date)).in_hours
+    time_from_commit_to_aptly = time_from_commit_to_release_prep - time_from_aptly_to_release_prep
+
+    puts "#{sha},#{commit_date},#{build_tag_date},#{release_tag_date},#{parsed_release_tag_date.month}/#{parsed_release_tag_date.year},#{time_from_aptly_to_release_prep},#{time_from_commit_to_release_prep},#{'%.2f' % time_from_commit_to_aptly},#{release_tag},#{repo_name}"
   end
 end
--- generate_logs.sh
+++ generate_logs.sh
@@ -7,7 +7,7 @@ repos=$(cat ./repo_list.txt)
 log_file_name="time_from_commit_to_release_logs.csv"
 
 [ -f $log_file_name ] && rm $log_file_name
-echo "Sha,Commit date,Tag date,Commit to release prep (hours), Aptly to release prep (hours),Tag,Repo" > $log_file_name
+echo "Sha,Commit date,Build tag date,Release tag date,Release month,Aptly to release prep (hours),Commit to release prep (hours),Commit to aptly (hours),Tag,Repo" > $log_file_name
 
 for repo in $repos
 do
