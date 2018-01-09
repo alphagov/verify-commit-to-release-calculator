c2bc823bb42a48bf65f2cd231e32a314f73f83b8
Keerthana Subramanian <keerthana.subramanian@digital.cabinet-office.gov.uk>
2018-01-09 15:53:46 +0000

    BAU: Moving logic from shell script to ruby
    - extracting the release time from release log
    - generating csv file per repository
    
    Authors: @SKeerthana @adrianw1832

--- calculate_difference.rb
+++ calculate_difference.rb
@@ -11,26 +11,23 @@ def get_tag(sha, tag)
   `git tag --contains #{sha}| egrep #{regex} | sort -t _ -k 2 -g | head -1`.chomp
 end
 
-repo_name = ARGV[0]
-shas = `git log --after=#{STARTDATE} --format=format:%H -#{NUMBEROFCOMMITS}`.split("\n")
+def compute_difference(repo_name)
+  return_value = []
+  shas = `git log --after=#{STARTDATE} --format=format:%H -#{NUMBEROFCOMMITS}`.split("\n")
+  shas.each do |sha|
+    release_tag = get_tag(sha, 'release')
 
-shas.each do |sha|
-  release_tag = repo_name == 'verify-puppet' ? get_tag(sha, 'verify_puppet_release') : get_tag(sha, 'release')
-
-  unless release_tag.empty?
-    commit_date = `TZ=UTC git log -1 --format=%cd --date=local #{sha}`.chomp
-
-    build_tag = get_tag(sha, 'build')
-    build_tag_number = build_tag.delete("^0-9")
-    build_tag_date = `git tag -l --format="%(taggerdate:format:%c)" #{build_tag}`.chomp
-
-    release_tag_date = `git tag -l --format="%(taggerdate:format:%c)" #{release_tag}`.chomp
-    parsed_release_tag_date = DateTime.parse(release_tag_date)
-
-    time_from_commit_to_release_prep = TimeDifference.between(parsed_release_tag_date, DateTime.parse(commit_date)).in_hours
-    time_from_aptly_to_release_prep = TimeDifference.between(parsed_release_tag_date, DateTime.parse(build_tag_date)).in_hours
-    time_from_commit_to_aptly = time_from_commit_to_release_prep - time_from_aptly_to_release_prep
-
-    puts "#{sha},#{commit_date},#{build_tag_date},#{release_tag_date},#{parsed_release_tag_date.month}/#{parsed_release_tag_date.year},#{time_from_aptly_to_release_prep},#{time_from_commit_to_release_prep},#{'%.2f' % time_from_commit_to_aptly},#{release_tag},#{repo_name}"
+    unless release_tag.empty?
+      commit_date = `TZ=UTC git log -1 --format=%cd --date=local #{sha}`.chomp
+      parsed_repo_name=repo_name.tr('-', ' ')
+      release_tag_number = release_tag.delete("^0-9")
+      release_date = `find ../verify-release-logs -maxdepth 1 -type d -execdir git grep -i -h -e "#{parsed_repo_name}" --and -e '"#{release_tag_number}"' --and -e "Send release complete email" \\; | jq ".end_time"`.chomp
+      if !release_date.empty?
+          parsed_release_date = DateTime.parse(release_date)
+          time_from_commit_to_release = TimeDifference.between(parsed_release_date, DateTime.parse(commit_date)).in_hours
+          return_value << "#{sha},#{commit_date},#{release_date},#{time_from_commit_to_release},#{release_tag}"
+      end
+    end
   end
+  return_value
 end
---
+++ generate_logs.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+require './calculate_difference.rb'
+
+def git_clone(repo_name)
+  if File.directory?(repo_name)
+    Dir.chdir repo_name
+    `git pull --rebase`
+  else
+    `git clone "git@github.com:alphagov/#{repo_name}"`
+    Dir.chdir repo_name
+  end
+end
+
+current_directory = Dir.pwd
+git_clone("verify-release-logs")
+Dir.chdir current_directory
+
+repos = File.read("repo_list.txt").split("\n")
+repos.each do |repo|
+  git_clone(repo)
+
+  log_file_name="time_from_commit_to_release_#{repo}.csv"
+  calculated_values = ["Sha,Commit date,Release date,Commit to release (hours),Tag"] + compute_difference(repo)
+
+  IO.write(log_file_name, calculated_values.join("\n"))
+  Dir.chdir current_directory
+  puts "Done with : " + repo
+end
+
