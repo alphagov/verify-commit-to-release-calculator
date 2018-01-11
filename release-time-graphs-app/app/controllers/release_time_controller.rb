e925e7abe5a18293384f58b4037496db71ef6390
Keerthana Subramanian <keerthana.subramanian@digital.cabinet-office.gov.uk>
2018-01-11 12:19:01 +0000

    Adding commit to aptly time in the graphs

--- release-time-graphs-app/app/controllers/release_time_controller.rb
+++ release-time-graphs-app/app/controllers/release_time_controller.rb
@@ -16,24 +16,28 @@ class ReleaseTimeController < ApplicationController
     file = "../#{CSV_FOLDER}/#{params['repo']}.csv"
     return render :error unless File.exists?(file)
 
-    @commits = load_commits_from_csv file
-    @commits_lead_time_graph = @commits.map{|commit| [commit.commit_date, commit.lead_time] }
-    @average_lead_time_per_week_graph = average_lead_time_per_week @commits
+    @commits = load_commits_from_csv(file).select{|commit| commit.commit_to_release_lead_time < 1000 }
+    @commits_to_release_lead_time_graph = @commits.map{|commit| [commit.commit_date, commit.commit_to_release_lead_time] }
+    @commits_to_build_lead_time_graph = @commits.map{|commit| [commit.commit_date, commit.commit_to_build_lead_time] }
+    @average_build_lead_time_per_week_graph = average_lead_time_per_week(@commits, :commit_to_build_lead_time)
+    @average_release_lead_time_per_week_graph = average_lead_time_per_week(@commits, :commit_to_release_lead_time)
   end
 
-  def average_lead_time_per_week commits
+  def average_lead_time_per_week(commits, tag_type)
     average_lead_time_per_week=[]
     weekly_sum = 0
     weekly_count=0
     current_week_beginning_date= DateTime.now.weeks_ago(1)
     commits.each() do |commit|
-      if(commit.commit_date > current_week_beginning_date )
-        weekly_sum += commit.lead_time
+      lead_time = commit.send(tag_type)
+
+      if(commit.commit_date > current_week_beginning_date)
+        weekly_sum += lead_time
         weekly_count = weekly_count + 1
       else
         average_lead_time_per_week << [current_week_beginning_date,  weekly_count == 0?  0.00 : weekly_sum/weekly_count]
         weekly_count = 1
-        weekly_sum = commit.lead_time
+        weekly_sum = lead_time
         current_week_beginning_date= current_week_beginning_date.weeks_ago(1)
       end
     end
@@ -48,7 +52,8 @@ class ReleaseTimeController < ApplicationController
         commit_date: DateTime.parse(commit['Commit date']),
         release_date: DateTime.parse(commit['Release date']),
         release_tag: commit['Tag'],
-        lead_time: commit['Commit to release (hours)'].to_f.round(2))
+        commit_to_release_lead_time: commit['Commit to release (hours)'].to_f.round(2),
+        commit_to_build_lead_time: commit['Commit to build(hours)'].to_f.round(2))
     end
     commits
   end
--- release-time-graphs-app/app/models/commit.rb
+++ release-time-graphs-app/app/models/commit.rb
@@ -1,10 +1,11 @@
 class Commit
-  attr_accessor :commit_date, :lead_time, :sha, :release_date, :release_tag
+  attr_accessor :commit_date, :commit_to_release_lead_time, :commit_to_build_lead_time, :sha, :release_date, :release_tag
 
   def initialize props
     @commit_date = props[:commit_date]
     @release_date = props[:release_date]
-    @lead_time = props[:lead_time]
+    @commit_to_build_lead_time = props[:commit_to_build_lead_time]
+    @commit_to_release_lead_time = props[:commit_to_release_lead_time]
     @sha = props[:sha]
     @release_tag = props[:release_tag]
   end
