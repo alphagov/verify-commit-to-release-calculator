4679ac506549aba5c12c2471ef4781355ec8485b
Minno Dang <minh.ngocdang@digital.cabinet-office.gov.uk>
2018-01-12 17:21:08 +0000

    Refactorings to the ruby scripts. Pulling repos into a repos folder so we don't have to explicitly gitignore each repo

--- calculate_difference.rb
+++ calculate_difference.rb
@@ -4,44 +4,51 @@ require 'date'
 require 'time_difference'
 require './repo_to_release_log_name.rb'
 
+RELEASE_COMPLETION_STEP = 'Send release complete email'
 
 def get_tags(sha, tag)
-  regex = tag == 'build' ? "^'#{tag}_\\d{2,}'" : "'#{tag}_\\d{2,}'"
+  regex = "'#{tag}_\\d{2,}'"
+  regex = regex.prepend '^' if tag == :build
   `git tag --contains #{sha}| egrep #{regex} | sort -t _ -k 2 -g`.split("\n")
+end
+
+def grep_release_date(log_name, release_tag_number)
+  `find ../verify-release-logs -maxdepth 1 -type d -execdir git grep -i -h -e "#{log_name}" --and -e '"#{release_tag_number}"' --and -e "#{RELEASE_COMPLETION_STEP}" \\; | jq ".end_time"`.chomp
 end
 
 def get_release_date(release_tag, sha, repo_name)
   log_name = REPO_TO_VERIFY_LOGS_MAPPING[repo_name]
-  release_tag_number = release_tag.delete("^0-9")
-  release_date = `find ../verify-release-logs -maxdepth 1 -type d -execdir git grep -i -h -e "#{log_name}" --and -e '"#{release_tag_number}"' --and -e "Send release complete email" \\; | jq ".end_time"`.chomp
+  release_tag_number = release_tag.delete('^0-9')
+  release_date = grep_release_date(log_name, release_tag_number)
 
-  unless release_date.empty?
-    commit_date = `TZ=UTC git log -1 --format=%cd --date=local #{sha}`.chomp
+  return if release_date.empty?
 
-    build_tag = get_tags(sha, 'build')[0]
-    build_date = `git tag -l --format="%(taggerdate:format:%c)" #{build_tag}`.chomp
+  commit_date = `TZ=UTC git log -1 --format=%cd --date=local #{sha}`.chomp
 
-    parsed_build_date = DateTime.parse(build_date)
-    parsed_release_date = DateTime.parse(release_date)
+  build_tag = get_tags(sha, :build)[0]
+  build_date = `git tag -l --format="%(taggerdate:format:%c)" #{build_tag}`.chomp
 
-    time_from_commit_to_build = TimeDifference.between(parsed_build_date, DateTime.parse(commit_date)).in_hours
-    time_from_commit_to_release = TimeDifference.between(parsed_release_date, DateTime.parse(commit_date)).in_hours
-    [sha,commit_date,build_date,release_date,time_from_commit_to_build,time_from_commit_to_release,release_tag].join(',')
+  parsed_build_date = DateTime.parse(build_date)
+  parsed_release_date = DateTime.parse(release_date)
+
+  time_from_commit_to_build = TimeDifference.between(parsed_build_date, DateTime.parse(commit_date)).in_hours
+  time_from_commit_to_release = TimeDifference.between(parsed_release_date, DateTime.parse(commit_date)).in_hours
+  [sha,commit_date,build_date,release_date,time_from_commit_to_build,time_from_commit_to_release,release_tag].join(',')
+end
+
+def compute_difference_for_sha(sha, repo_name)
+  release_tags = get_tags(sha, :release)
+  release_tags.each do |release_tag|
+    line = get_release_date(release_tag, sha, repo_name)
+    return line if line
   end
 end
 
 def compute_difference(repo_name)
-  return_value = []
   shas = `git log --after="1 year ago" --format=format:%H`.split("\n")
-
-  shas.each do |sha|
-    release_tags = get_tags(sha, 'release')
-    release_tags.each do |release_tag|
-      line = get_release_date(release_tag, sha, repo_name)
-      next unless line
-      return_value << line
-      break
-    end
-  end
-  return_value
+  puts "SHA count: #{shas.count}"
+  shas.map do |sha|
+    putc '.'
+    compute_difference_for_sha sha, repo_name
+  end.compact
 end
--- generate_logs.rb
+++ generate_logs.rb
@@ -2,31 +2,37 @@
 
 require './calculate_difference.rb'
 
-def git_clone(repo_name)
-  if File.directory?(repo_name)
-    Dir.chdir repo_name
-    `git pull --rebase`
-  else
-    `git clone "git@github.com:alphagov/#{repo_name}"`
-    Dir.chdir repo_name
-  end
+HEADERS = 'Sha,Commit date,Build date,Release date,Commit to build(hours),Commit to release (hours),Tag'
+
+ROOT_DIR = Dir.pwd
+OUTPUT_DIR = ROOT_DIR + '/release-time-graphs-app/output'
+REPOS_DIR = ROOT_DIR + '/repos'
+REPO_LIST = ROOT_DIR + '/repo_list.txt'
+
+def git_clone(repo)
+  Dir.mkdir REPOS_DIR unless File.directory? REPOS_DIR
+  Dir.chdir REPOS_DIR
+
+  File.directory?(repo) ?
+    `cd #{repo} && git pull --rebase` :
+    `git clone git@github.com:alphagov/#{repo}`
+
+  Dir.chdir repo
 end
 
-current_directory = Dir.pwd
-git_clone("verify-release-logs")
-Dir.chdir current_directory
+git_clone('verify-release-logs')
+repos = File.read(REPO_LIST).split("\n")
 
-repos = File.read("repo_list.txt").split("\n")
 repos.each do |repo|
   git_clone(repo)
-  puts "Calculating values for repository : " + repo
-  log_file_name = "time_from_commit_to_release_#{repo}.csv"
-  calculated_values = ["Sha,Commit date,Build date,Release date,Commit to build(hours),Commit to release (hours),Tag"] + compute_difference(repo)
 
-  output_dir_name = "../release-time-graphs-app/output"
-  Dir.mkdir output_dir_name if !File.directory? output_dir_name
-  IO.write("#{output_dir_name}/#{log_file_name}", calculated_values.join("\n"))
-  Dir.chdir current_directory
-  puts "Done with : #{repo}"
+  puts 'Calculating values for repository : ' + repo
+
+  log_file_name = "time_from_commit_to_release_#{repo}.csv"
+  calculated_values = [HEADERS] + compute_difference(repo)
+
+  Dir.mkdir OUTPUT_DIR unless File.directory? OUTPUT_DIR
+  IO.write("#{OUTPUT_DIR}/#{log_file_name}", calculated_values.join("\n"))
+  puts "\nDone with : #{repo}"
 end
 
